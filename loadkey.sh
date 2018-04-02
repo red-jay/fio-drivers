8965e8ce3735b9664b5ce9b6f214b7deab43b678
RJ Bergeron <rbergero@gmail.com>
2018-04-01 20:38:57 -0400

    add key loader

---
+++ loadkey.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set +x
+
+{
+  echo "-----BEGIN PGP PRIVATE KEY BLOCK-----"
+  echo "${GPG_SECRET}" | fold -w65
+  echo "-----END PGP PRIVATE KEY BLOCK-----"
+} | gpg --import
