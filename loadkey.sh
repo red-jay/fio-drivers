4af1cbeb4a062b4abda424747b1f3f63d0dd0131
RJ Bergeron <rbergero@gmail.com>
2018-04-01 20:47:16 -0400

    better match real keys

--- loadkey.sh
+++ loadkey.sh
@@ -4,6 +4,7 @@ set +x
 
 {
   echo "-----BEGIN PGP PRIVATE KEY BLOCK-----"
-  echo "${GPG_SECRET}" | fold -w65
+  echo ""
+  echo "${GPG_SECRET}" | fold -w64
   echo "-----END PGP PRIVATE KEY BLOCK-----"
 } | gpg --import || true
