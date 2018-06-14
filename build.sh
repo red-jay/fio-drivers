50c6f04c44776c5410fb145b523a913f00f6bf8f
RJ Bergeron <rbergero@gmail.com>
2018-06-14 19:21:50 -0400

    add github key

--- build.sh
+++ build.sh
@@ -49,6 +49,9 @@ cd "${dkms2ppa_dir}"
 # publish apt repo
 cd "${srcdir}"
 
+mkdir -p "${HOME}/.ssh"
+echo "github.com,192.30.253.112 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> "${HOME}/.ssh/known_hosts"
+
 apt-get -qq update && apt-get -qq -y install git
 git branch --track gh-pages remotes/origin/gh-pages
 gh_pages=$(mktemp -d)
